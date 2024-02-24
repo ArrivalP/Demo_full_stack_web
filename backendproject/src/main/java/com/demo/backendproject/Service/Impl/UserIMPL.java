62736e3cc44efe9f869a32c6e1a93d71180b7708
Pakin Thaisamark <pakin.t@asnbroker.co.th>
2024-02-24 13:05:38 +0700

    add login function draft 1

--- backendproject/src/main/java/com/demo/backendproject/Service/Impl/UserIMPL.java
+++ backendproject/src/main/java/com/demo/backendproject/Service/Impl/UserIMPL.java
@@ -18,38 +18,37 @@ public class UserIMPL implements UserService {
     private UserRepo userRepo;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Override
     public String addUser(UserDto userDTO) {
         UserEntity userEntity = new UserEntity(
                 userDTO.getUser_id(),
                 userDTO.getName(),
                 userDTO.getEmail(),
-                this.passwordEncoder.encode(userDTO.getPassword())
-        );
+                this.passwordEncoder.encode(userDTO.getPassword()));
         userRepo.save(userEntity);
         return userEntity.getName();
     }
 
     @Override
     public LoginResponse loginUser(LoginDto loginDto) {
-        String msg ="";
-        UserEntity user1 =userRepo.findByEmail(loginDto.getEmail());
-        if(user1 != null){
+        UserEntity user1 = userRepo.findByEmail(loginDto.getEmail());
+        if (user1 != null) {
             String password = loginDto.getPassword();
             String encodedpassword = user1.getPassword();
-            Boolean isPwdRight = passwordEncoder.matches(password,encodedpassword);
-            if(isPwdRight){
-                Optional<UserEntity> user = userRepo.findOneByEmailAndPassword(loginDto.getEmail(),encodedpassword);
-                if(user.isPresent()){
-                    return  new LoginResponse("Login Success",true);
-                }else{
-                    return  new LoginResponse("Login Failed",false);
+            Boolean isPwdRight = passwordEncoder.matches(password, encodedpassword);
+            if (isPwdRight) {
+                Optional<UserEntity> user = userRepo.findOneByEmailAndPassword(loginDto.getEmail(), encodedpassword);
+                if (user.isPresent()) {
+                    return new LoginResponse("Login Success", true);
+                } else {
+                    return new LoginResponse("Login Failed", false);
                 }
-            }else {
-                return  new LoginResponse("Password not Match",false);
+            } else {
+                return new LoginResponse("Password not Match", false);
             }
-        }else{
-            return  new LoginResponse("Email Not Exists",false);
+        } else {
+            return new LoginResponse("Email Not Exists", false);
         }
     }
 
